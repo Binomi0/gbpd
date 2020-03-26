263e0fca7f775f834c6887277614b66f06ce753e
Adolfo Onrubia <adolfo@onrubia.es>
2020-03-26 21:52:45 +0100

    first commit

--- mongo/init-mongo.js
+++ mongo/init-mongo.js
@@ -6,10 +6,10 @@ console.log("#######################")
 
 const gbpd = {
   user: "gbpd",
+  db: "dev",
   roles: [
     { role: "clusterAdmin", db: "dev" },
     { role: "readAnyDatabase", db: "dev" },
-    "readWrite"
   ],
   pwd: "edec408fac172f177696b9e97a6e1be6447d0bbe",
   mechanisms: ["SCRAM-SHA-1"],
