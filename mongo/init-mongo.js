8b6e7448c9cf2e7bb1cbf344b10ee1f43dd58df5
Adolfo Onrubia <adolfo@onrubia.es>
2020-03-26 14:48:58 +0100

    add docker compose config files

---
+++ mongo/init-mongo.js
@@ -0,0 +1,31 @@
+console.log("#######################")
+console.log("#######################")
+console.log("#######  TEST   #######")
+console.log("#######################")
+console.log("#######################")
+
+const gbpd = {
+  user: "gbpd",
+  roles: [
+    { role: "clusterAdmin", db: "dev" },
+    { role: "readAnyDatabase", db: "dev" },
+    "readWrite"
+  ],
+  pwd: "edec408fac172f177696b9e97a6e1be6447d0bbe",
+  mechanisms: ["SCRAM-SHA-1"],
+};
+
+// const admin = {
+//   user: "admin",
+//   pwd: "7110eda4d09e062aa5e4a390b0a572ac0d2c0220",
+//   mechanisms: ["SCRAM-SHA-256"],
+//   roles: [
+//     { role: "clusterAdmin", db: "dev" },
+//     { role: "readAnyDatabase", db: "dev" },
+//     "readWrite"
+//   ]
+// }
+
+db.createUser(gbpd);
+
+db.collection.users.create({ email: 'adolfo@onrubia.es', password: 'Aoar12003'})
