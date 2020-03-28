2b4d28df7aa60a4e976f1a3cdacafb4ccde38151
Adolfo Onrubia <adolfo@onrubia.es>
2020-03-29 00:38:01 +0100

    more changes

--- .docker/mongodb/initdb.d/mongo.js
+++ .docker/mongodb/initdb.d/mongo.js
@@ -1,8 +1,8 @@
 db.createUser({
-  user: "gbpd",
+  user: "adolfo",
   pwd: "edec408fac172f177696b9e97a6e1be6447d0bbe",
   mechanisms: ["SCRAM-SHA-1"],
   roles: [
-    { role: "dbAdmin", db: "dev" }
+    { role: "dbAdmin", db: "admin" }
   ]
 });
