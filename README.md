cca13f4344a7995d775802363b717a3e3c5bc870
Adolfo Onrubia <adolfo@onrubia.es>
2020-03-28 18:47:18 +0100

    more changes

---
+++ README.md
@@ -0,0 +1,12 @@
+```js
+rs.initiate(
+  {
+    _id : 'rs0',
+    members: [
+      { _id : 0, host : "mongo:27017" },
+      { _id : 1, host : "mongo2:27017" },
+      { _id : 2, host : "mongo3:27017" }
+    ]
+  }
+)
+```
--- create-user.js
+++ create-user.js
@@ -3,3 +3,6 @@ db.createUser({
   pwd: process.env.MONGO_INITDB_PASSWORD,
   roles: [ { role: "dbAdmin", db: process.env.MONGO_INITDB_DATABASE } ]
 });
+
+
+rs.initiate( { _id: 'rs0', members: [ { _id: 0, host: "mongo:27017" },{ _id: 1, host: "mongo2:27017" },{ _id: 2, host: "mongo3:27017" } ] } );
